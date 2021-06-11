6baa8e19057880d67c5245c1aaecbaf248330728
Bryce Glover <RandomDSdevel@gmail.com>
2021-06-11 19:53:12 -0400

    src/AppDelegate.h:  Create it as a new file.
         Write my app delegate header file.

---
+++ src/AppDelegate.h
@@ -0,0 +1,10 @@
+//
+// AppDelegate.h
+// network-watchexec
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject<NSApplicationDelegate>
+  @property(nonatomic, copy) NSString* current_SSID;
+@end
